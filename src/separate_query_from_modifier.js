cd69839f3e088b2970885e9ea2631b955b81a2cf
Jihyun Yoon <yolha79@gmail.com>
2019-02-16 16:34:39 +0900

    Copy the function, naming it after the query aspect of the function.

--- src/separate_query_from_modifier.js
+++ src/separate_query_from_modifier.js
@@ -1,6 +1,20 @@
 function setOffAlarms () {
 }
 
+function findMiscreant (people) {
+  for (const p of people) {
+    if (p === "Don") {
+      setOffAlarms();
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return ""
+}
+
 function alertForMiscreant (people) {
   for (const p of people) {
     if (p === "Don") {
