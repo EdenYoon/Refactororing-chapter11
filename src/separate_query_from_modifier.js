002223f919fe2fc67cd25e410e4e2b14f7773434
Jihyun Yoon <yolha79@gmail.com>
2019-02-16 16:08:45 +0900

    Add separate_query_from_modifier.js

---
+++ src/separate_query_from_modifier.js
@@ -0,0 +1,16 @@
+function setOffAlarms () {
+}
+
+function alertForMiscreant (people) {
+  for (const p of people) {
+    if (p === "Don") {
+      setOffAlarms();
+      return "Don";
+    }
+    if (p === "John") {
+      setOffAlarms();
+      return "John";
+    }
+  }
+  return "";
+}
