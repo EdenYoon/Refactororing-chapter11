04dc75dc8d44e4047b5d513e689f990c4b0781d9
Jihyun Yoon <yolha79@gmail.com>
2019-02-16 16:35:53 +0900

    Remove the side effects from this new query

--- src/separate_query_from_modifier.js
+++ src/separate_query_from_modifier.js
@@ -4,11 +4,9 @@ function setOffAlarms () {
 function findMiscreant (people) {
   for (const p of people) {
     if (p === "Don") {
-      setOffAlarms();
       return "Don";
     }
     if (p === "John") {
-      setOffAlarms();
       return "John";
     }
   }
